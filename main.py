a4ed0d1dac4c7361e96e236b8a27e352725e7d87
notCamelCaseName <46199767+notCamelCaseName@users.noreply.github.com>
2022-09-30 00:37:42 +0200

    Fixed confusion between add and create

--- main.py
+++ main.py
@@ -8,18 +8,17 @@ def main(op, args):
     if op == "init":
         init()
     elif op == "add":
-        course, name, note_type = args
-        filepath = "note_files/" + course + "::" + name + ".md"
-        add(course, name, note_type, filepath)
-    elif op == "edit":
-        course, name = args[0:2]
-        edit(course, name)
-    elif op == "create":
         course, name, note_type = args[0:3]
         if len(args) > 3:
             filepath = args[4]
         else:
             filepath = "note_files/" + course + "::" + name + ".md"
+        add(course, name, note_type, filepath)
+    elif op == "edit":
+        course, name = args[0:2]
+        edit(course, name)
+    elif op == "create":
+        course, name, note_type = args
         add(course, name, note_type, filepath)
         edit(course, name)
     elif op == "list":
